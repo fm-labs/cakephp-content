698f8c89eabc952693dceeb85859c1473c4d0e78
fm-labs <flowmotionlabs@gmail.com>
2016-10-27 20:39:49 +0200

    RedirectPageType->getUrl() returns redirect location

--- src/Page/RedirectPageType.php
+++ src/Page/RedirectPageType.php
@@ -1,6 +1,13 @@
 <?php
 namespace Content\Page;
 
+use Cake\Routing\Router;
+
 class RedirectPageType extends AbstractPageType
 {
+    public function getUrl()
+    {
+        $url = $this->page->redirect_location;
+        return Router::url($url, true);
+    }
 }
